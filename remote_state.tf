0a3e858f7e62c0e615b3be1e7d3669b92eec56bc
David Joo <david.joo@hashicorp.com>
2025-07-08 12:54:10 +1000

    Refactor: Update execution role ARN to source from remote state data

--- remote_state.tf
+++ remote_state.tf
@@ -10,11 +10,24 @@ data "terraform_remote_state" "network" {
   }
 }
 
+# AWS IAM role workspace for execution roles
+data "terraform_remote_state" "aws_role" {
+  backend = "remote"
+  
+  config = {
+    organization = "djoo-hashicorp"
+    
+    workspaces = {
+      name = "aws-openid-role-for-stacks"
+    }
+  }
+}
+
 locals {
   vpc_id            = data.terraform_remote_state.network.outputs.vpc_id
   subnet_ids        = data.terraform_remote_state.network.outputs.vpc_public_subnets
   security_group_id = data.terraform_remote_state.network.outputs.security_group-ssh_http_https_allowed
 
   # Leveraging AWS OIDC variable set
-  execution_role_arn = var.TFC_AWS_RUN_ROLE_ARN
+  execution_role_arn = data.terraform_remote_state.aws_role.outputs.role_arn
 }
