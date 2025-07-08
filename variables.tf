5d528d786b9756ba344854ae586961110a7501ed
David Joo <david.joo@hashicorp.com>
2025-07-08 12:57:33 +1000

    Refactor: Remove unused AWS execution role ARN variable from variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "TFC_AWS_RUN_ROLE_ARN" {
-  description = "AWS execution role ARN"
-  type        = string
-  sensitive   = true
-}
-
 variable "container_image" {
   description = "Container image for website"
   type        = string
